81474aada603e3ff520ee7f0dc41a8d564dba36a
Mochamad Arifin <klampok.child@gmail.com>
2020-07-09 11:50:47 +0700

    fix link

--- test/test.mk
+++ test/test.mk
@@ -1,9 +1,9 @@
-TEST_SOURCES    := $(wildcard test/*.c)
+TEST_SOURCES    := $(wildcard test/test_*.c)
 TEST_OBJECTS    := $(patsubst %.c, build/%.o, $(TEST_SOURCES))
-TEST_BINS       := $(filter-out build/test, $(patsubst build/test/%.o, build/%, $(TEST_OBJECTS)))
-MKDIRS          += build/test
+TEST_BINS       := $(patsubst test/%.c, build/%, $(TEST_SOURCES))
 OBJECTS_NO_MAIN := $(filter-out build/whatsappd.o, $(OBJECTS))
 
+MKDIRS          += build/test
 
 test: $(TEST_BINS)
 	@for test_bin in $(TEST_BINS); do \
@@ -16,10 +16,10 @@ test-watch:
 		-e .c,.h,.mk -V \
 		-x "make --no-print-directory test || false"
 
-$(TEST_BINS): build/test_%: build/test/test_%.o build/test/test.o $(OBJECTS_NO_MAIN)
-	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^
+$(TEST_BINS): build/test_%: test/test.c build/test/test_%.o $(OBJECTS_NO_MAIN)
+	$(CC) $(CFLAGS) -Isrc "-DTEST=\"$*\"" $(LDFLAGS) -o $@ $^
 
-$(TEST_OBJECTS): build/test/%.o: test/%.c
+$(TEST_OBJECTS): build/%.o: %.c
 	$(CC) $(CFLAGS) -Isrc "-DTEST=\"$*\"" -c -o $@ $<
 
 .PHONY: test test-watch
