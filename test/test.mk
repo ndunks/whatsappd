b8b13b16cefc2b38c381adbf394efa7696dd4d94
Mochamad Arifin <klampok.child@gmail.com>
2020-07-09 15:23:53 +0700

    selectable TEST

--- test/test.mk
+++ test/test.mk
@@ -1,8 +1,14 @@
-TEST_SOURCES    := $(wildcard test/test_*.c)
+ifdef TEST
+    TEST_SOURCES:= test/test_$(TEST).c
+else
+    TEST_SOURCES:= $(wildcard test/test_*.c)
+endif
+
+TEST            ?= $*
+TEST_CFLAGS      = "-DTEST=\"$(TEST)\""
 TEST_OBJECTS    := $(patsubst %.c, build/%.o, $(TEST_SOURCES))
 TEST_BINS       := $(patsubst test/%.c, build/%, $(TEST_SOURCES))
 OBJECTS_NO_MAIN := $(filter-out build/whatsappd.o, $(OBJECTS))
-
 MKDIRS          += build/test
 
 test: $(LIB_OBJECTS) $(OBJECTS_NO_MAIN) $(TEST_BINS)
@@ -17,9 +23,9 @@ test-watch:
 		-x "make --no-print-directory test || false"
 
 $(TEST_BINS): build/test_%: test/test.c build/test/test_%.o $(OBJECTS_NO_MAIN) $(BUILD_LIB)
-	$(CC) $(CFLAGS) -Isrc "-DTEST=\"$*\"" $(LDFLAGS) -o $@ $^
+	$(CC) $(CFLAGS) $(TEST_CFLAGS) -Isrc $(LDFLAGS) -o $@ $^
 
-$(TEST_OBJECTS): build/%.o: %.c
-	$(CC) $(CFLAGS) -Isrc "-DTEST=\"$*\"" -c -o $@ $<
+$(TEST_OBJECTS): build/test/test_%.o: test/test_%.c
+	$(CC) $(CFLAGS) $(TEST_CFLAGS) -Isrc -c -o $@ $<
 
 .PHONY: test test-watch
