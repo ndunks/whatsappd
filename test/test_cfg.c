7996c45406b2e10d19ba426e4122435bedf16f8d
Mochamad Arifin <klampok.child@gmail.com>
2020-07-14 16:11:50 +0700

    fix ptr

--- test/test_cfg.c
+++ test/test_cfg.c
@@ -2,14 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <cfg.h>
-#include "test.h"
+#include <helper.h>
 
-void random_bytes(void *bytes, size_t size)
-{
-    srand(time(0) + size + rand());
-    while (size--)
-        ((char *)bytes)[size] = rand();
-}
+#include "test.h"
 
 int test_main()
 {
@@ -21,45 +16,45 @@ int test_main()
 
     cfg.cfg_file_version = 0x99;
 
-    random_bytes(&cfg.client_id, 16);
+    helper_random_bytes(16, cfg.client_id);
     ZERO(cfg_save(&cfg));
     ZERO(cfg_load(cfg2));
 
     TRUTHY(cfg.cfg_file_version == cfg2->cfg_file_version);
-    ZERO(memcmp(&cfg.client_id, &cfg2->client_id, 16));
+    ZERO(memcmp(cfg.client_id, cfg2->client_id, 16));
 
     memset(cfg2, 0, sizeof(CFG));
 
-    random_bytes(&cfg.keys.private, 32);
-    random_bytes(&cfg.keys.public, 32);
-    random_bytes(&cfg.keys.secret, 32);
-    random_bytes(&cfg.serverSecret, 144);
-    random_bytes(&cfg.aesKey, 32);
-    random_bytes(&cfg.macKey, 32);
+    helper_random_bytes(32, cfg.keys.private);
+    helper_random_bytes(32, cfg.keys.public);
+    helper_random_bytes(32, cfg.keys.secret);
+    helper_random_bytes(144, cfg.serverSecret);
+    helper_random_bytes(32, cfg.aesKey);
+    helper_random_bytes(32, cfg.macKey);
 
-    strcpy((char *)&cfg.tokens.client, "%#^$#%^$#&$^#&$*#&$#)(*");
-    strcpy((char *)&cfg.tokens.server, "636tgyf7ryfhr7ryhf7rufhrg8rgrgj8rgj");
-    strcpy((char *)&cfg.tokens.browser, "||ASDFGHJKDFKDFKDKFKDFKFDK636tgyf7ryfhr7ryhf7rufhrg8rgrgj8rgj%#^$#%^$#&$^#&$*#&$#)(*||");
+    strcpy(cfg.tokens.client, "%#^$#%^$#&$^#&$*#&$#)(*");
+    strcpy(cfg.tokens.server, "636tgyf7ryfhr7ryhf7rufhrg8rgrgj8rgj");
+    strcpy(cfg.tokens.browser, "||ASDFGHJKDFKDFKDKFKDFKFDK636tgyf7ryfhr7ryhf7rufhrg8rgrgj8rgj%#^$#%^$#&$^#&$*#&$#)(*||");
 
     ZERO(cfg_save(&cfg));
     ZERO(cfg_load(cfg2));
 
     TRUTHY(cfg.cfg_file_version == cfg2->cfg_file_version);
-    ZERO(memcmp(&cfg.client_id, &cfg2->client_id, 16));
-    ZERO(memcmp(&cfg.keys.private, &cfg2->keys.private, 32));
-    ZERO(memcmp(&cfg.keys.public, &cfg2->keys.public, 32));
-    ZERO(memcmp(&cfg.keys.secret, &cfg2->keys.secret, 32));
+    ZERO(memcmp(cfg.client_id, cfg2->client_id, 16));
+    ZERO(memcmp(cfg.keys.private, cfg2->keys.private, 32));
+    ZERO(memcmp(cfg.keys.public, cfg2->keys.public, 32));
+    ZERO(memcmp(cfg.keys.secret, cfg2->keys.secret, 32));
 
-    ZERO(memcmp(&cfg.aesKey, &cfg2->aesKey, 32));
-    ZERO(memcmp(&cfg.macKey, &cfg2->macKey, 32));
+    ZERO(memcmp(cfg.aesKey, cfg2->aesKey, 32));
+    ZERO(memcmp(cfg.macKey, cfg2->macKey, 32));
 
-    ZERO(memcmp(&cfg.tokens.client, &cfg2->tokens.client, 60));
-    ZERO(memcmp(&cfg.tokens.server, &cfg2->tokens.server, 120));
-    ZERO(memcmp(&cfg.tokens.browser, &cfg2->tokens.browser, 180));
+    ZERO(memcmp(cfg.tokens.client, cfg2->tokens.client, 60));
+    ZERO(memcmp(cfg.tokens.server, cfg2->tokens.server, 120));
+    ZERO(memcmp(cfg.tokens.browser, cfg2->tokens.browser, 180));
 
-    ZERO(strcmp((char *)&cfg.tokens.client, (char *)&cfg2->tokens.client));
-    ZERO(strcmp((char *)&cfg.tokens.server, (char *)&cfg2->tokens.server));
-    ZERO(strcmp((char *)&cfg.tokens.browser, (char *)&cfg2->tokens.browser));
+    ZERO(strcmp(cfg.tokens.client, cfg2->tokens.client));
+    ZERO(strcmp(cfg.tokens.server, cfg2->tokens.server));
+    ZERO(strcmp(cfg.tokens.browser, cfg2->tokens.browser));
 
     free(cfg2);
     return 0;
@@ -68,7 +63,7 @@ int test_main()
 int test_setup()
 {
     char buf[255];
-    cfg_config_file = (char *)&"tmp/whatsappd.cfg";
+    cfg_config_file = "tmp/whatsappd.cfg";
     getcwd(buf, 255);
     info("PWD: %s", buf);
     return 0;
