7c18c3d75cd287f3fb8863de42c54102dbfb02ab
Mochamad Arifin <klampok.child@gmail.com>
2020-07-14 23:04:53 +0700

    refactor

--- lib/util/cfg.c
+++ lib/util/cfg.c
@@ -5,45 +5,6 @@
 #include "color.h"
 #include "cfg.h"
 
-/* #define BUF_SIZE 3480
-typedef enum cfg_maps {
-    cfg_field_client_id,
-
-    cfg_field_keys_secret,
-    cfg_field_keys_private,
-    cfg_field_keys_public,
-
-    cfg_field_tokens_client,
-    cfg_field_tokens_server,
-    cfg_field_tokens_browser,
-
-    cfg_field_serverSecret,
-    cfg_field_aesKey,
-    cfg_field_macKey
-
-} cfg_maps;
-
-typedef struct cfg_field
-{
-    unsigned char key_id, value_len;
-    char * value;
-} cfg_field;
-
-static char * buf;
- */
-
-// static int cfg_config_file_open(int flag){
-//     return open(cfg_config_file, flag);
-
-//     // struct passwd *pw = getpwuid(getuid());
-//     // const char *finalPath;
-//     // finalPath = malloc( strlen(pw->pw_dir) + 20 );
-//     // strcpy(finalPath, pw->pw_dir );
-//     // strcat(finalPath, cfg_config_file);
-
-//     // free(finalPath);
-//     // free(pw);
-// }
 char *cfg_config_file = NULL;
 
 int cfg_load(CFG *cfg)
